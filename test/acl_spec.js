7c450ad4d00df12748925d339ef07c32fa70c3d2
Matti Savolainen <matti@applifier.com>
2014-02-11 13:44:04 +0200

    initial refactor

--- test/acl_spec.js
+++ test/acl_spec.js
@@ -1,118 +1,64 @@
-var testSetup = require('./test_setup');
+require('./test_setup');
+var ACL = require('../lib/acl').ACL;
 var should = require('chai').should();
 var _ = require('lodash');
 var serverbone = require('..');
 
-var Db = require('backbone-db');
-var db = new Db('acl-test');
-
-var BaseModel = serverbone.models.BaseModel.extend({
-  sync: Db.sync.bind(db)
-});
-
-var UserModel = BaseModel.extend({
-});
-
-var TestModel = BaseModel.extend({
-  schema: {
-    owner: 'user_id',
-    access: {
-      read: ['owner'],
-      write: ['admin']
-    },
-    properties: {
-      name: {
-        type: 'string',
-        access: {
-          read: ['owner', 'admin'],
-          write: ['admin']
-        }
-      },
-      internal_id: {
-        type: 'string',
-        access: {
-          read: ['admin'],
-          write: ['admin']
-        }
-      }
-    }
-  }
-});
 
 describe('Test ACL', function () {
-  var anon = new UserModel({id: 'foo', admin: false});
-  var owner = new UserModel({id: 'user_id', admin: false});
-  var admin = new UserModel({id: 'admin_id', admin: true});
-  var foo = new TestModel({id: 1, name: 'foo', internal_id: 'xyz'});
-
-  it('should test hasPermission', function() {
-    var acl = new serverbone.acl.AccessControl({
-      read: ['owner', 'admin'],
-      write: ['admin']
-    });
-    acl.hasPermission(['owner'], 'read').should.equal(true);
-    acl.hasPermission(['owner'], 'write').should.equal(false);
-    acl.hasPermission(['admin'], 'read').should.equal(true);
-    acl.hasPermission(['admin'], 'write').should.equal(true);
-    acl.hasPermission(['world'], 'read').should.equal(false);
-  });
-
-  it('should allow only owner to read resource', function() {
-    foo.canAccess('read', anon).should.equal(false);
-    foo.canAccess('read', owner).should.equal(true);
-    foo.canAccess('write', owner).should.equal(false);
-    foo.canAccess('write', admin).should.equal(true);
-  });
-
-  it('should check property permissions lists', function() {
-    var userCanRead = foo.propertiesWithAccess('read', owner);
-    userCanRead.length.should.equal(1);
-    userCanRead[0].should.equal('name');
-    var userCanWrite = foo.propertiesWithAccess('write', owner);
-    userCanWrite.length.should.equal(0);
-
-    var adminCanWrite = foo.propertiesWithAccess('write', admin);
-    adminCanWrite.length.should.equal(2);
-    adminCanWrite.should.include('name');
-    adminCanWrite.should.include('internal_id');
-
-    var worldCanRead = foo.propertiesWithAccess('read', anon);
-    worldCanRead.length.should.equal(0);
-  });
-
-  it('toJSON should respect property permissions', function() {
-    var ownerJson = foo.toJSON({user: owner});
-    _.isEqual(ownerJson, {name: 'foo'}).should.equal(true);
-
-    var adminJson = foo.toJSON({user: admin});
-    _.isEqual(adminJson, {name: 'foo', internal_id: 'xyz'}).should.equal(true);
-
-    var worldJson = foo.toJSON({user: anon});
-    _.isEqual(worldJson, {}).should.equal(true);
-  });
-
-  it('it should not allow changing fields without permission', function(done) {
-    foo.set({internal_id: 'abc', 'name': 'new name'});
-    foo.save(foo.attributes, {user: owner})
-      .then(function() {
-        done(new Error('should not save'));
-      })
-      .otherwise(function(err) {
-        should.exist(err);
-        //TODO: check error
-        done();
+    describe('Access Roles', function () {
+    var acl;
+    before(function () {
+      acl = new ACL({
+        'owner': ['write', 'read', 'update'],
+        '*': ['read']
       });
+    });
+    it('should be possible to make ACL assertions agains roles and actions', function () {
+      acl.assert('owner', 'write').should.equal(true);
+      acl.assert('public', 'write').should.equal(false);
+      acl.assert('owner', 'delete').should.equal(false);
+      acl.assert('read').should.equal(true);
+      acl.assert('write').should.equal(false);
+    });
+    it('should be possible to add more rules with .allow', function () {
+      acl.grant({
+        'owner': ['delete'],
+        'admin': ['*']
+      });
+      acl.assert('admin', 'write').should.equal(true);
+      acl.assert('admin', 'read').should.equal(true);
+      acl.assert('admin', 'nonexisting').should.equal(true);
+      acl.assert('owner', 'delete').should.equal(true);
+    });
+    it('should be possible to grant and revoke access', function () {
+      acl.grant({
+        'tester': ['read', 'write'],
+        'owner': ['read', 'write', 'delete']
+      });
+      acl.assert('owner', 'delete').should.equal(true);
+      acl.revoke('owner');
+      acl.assert('owner', 'delete').should.equal(false);
+      acl.grant({
+        'owner': ['delete']
+      });
+      acl.assert('owner', 'delete').should.equal(true);
+      acl.revoke(['owner', 'user', 'tester']);
+      acl.assert('owner', 'delete').should.equal(false);
+    });
+    it('should recognice the "*" selector in roles and permissions', function () {
+      acl.grant({
+        'test': '*'
+      });
+      acl.assert('test', 'anything').should.equal(true);
+      acl.assert('test2', 'anything').should.equal(false);
+      acl.grant({
+        '*': 'abolish'
+      });
+      acl.assert('anyone', 'abolish').should.equal(true);
+      acl.assert('anyone', 'destroye').should.equal(false);
+    });
   });
-
-  it('admin should be allowed to write internal_id', function(done) {
-    foo.set({internal_id: 'abc', 'name': 'new name'});
-    foo.save(foo.attributes, {user: admin})
-      .then(function() {
-        done();
-      })
-      .otherwise(done);
-  });
-
 });
 
 
